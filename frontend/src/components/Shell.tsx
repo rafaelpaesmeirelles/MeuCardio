5eadf9cbd5c6c6a6f9feb248f964bcbeff667efb
rafaelpaesmeirelles <rafaelpaesmeirelles@gmail.com>
2026-08-12 18:47:05 -0300

    fix(ui): mount safe mail presentation normalization

--- frontend/src/components/Shell.tsx
+++ frontend/src/components/Shell.tsx
@@ -1,11 +1,13 @@
 import ShellClinicalOSLaunch from "./ShellClinicalOSLaunch";
 import ClinicalMobileNav from "./ClinicalMobileNav";
+import ClinicalPresentationGuard from "./ClinicalPresentationGuard";
 
 export default function Shell() {
   return (
     <>
       <ShellClinicalOSLaunch />
       <ClinicalMobileNav />
+      <ClinicalPresentationGuard />
     </>
   );
 }
