18ad9d9f4458bac36c9ddd3fdf5b377d78616f97
rafaelpaesmeirelles <rafaelpaesmeirelles@gmail.com>
2026-08-12 19:25:04 -0300

    feat(ui): align mobile navigation with canonical command center

--- frontend/src/components/ClinicalMobileNav.tsx
+++ frontend/src/components/ClinicalMobileNav.tsx
@@ -3,10 +3,6 @@ import Icone from "./Icone";
 import { IconeHoje } from "./IdentidadeClinica";
 
 export default function ClinicalMobileNav() {
-  function abrirAssistente() {
-    window.dispatchEvent(new Event("corvia:abrir-assistente-pessoal"));
-  }
-
   function abrirMais() {
     document.querySelector<HTMLButtonElement>(".cos-topbar__menu")?.click();
   }
@@ -17,18 +13,18 @@ export default function ClinicalMobileNav() {
         <IconeHoje />
         <span>Início</span>
       </NavLink>
+      <NavLink to="/busca" className="cc-mobile-nav__search">
+        <Icone nome="busca" />
+        <span>Buscar</span>
+      </NavLink>
       <NavLink to="/round">
         <Icone nome="pacientes" />
         <span>Pacientes</span>
       </NavLink>
-      <NavLink to="/busca" className="cc-mobile-nav__search">
-        <span className="cc-mobile-nav__search-icon"><Icone nome="busca" /></span>
-        <span>Buscar</span>
+      <NavLink to="/agenda">
+        <Icone nome="agenda" />
+        <span>Agenda</span>
       </NavLink>
-      <button type="button" onClick={abrirAssistente}>
-        <span className="cc-mobile-nav__assistant-icon">✦</span>
-        <span>Assistente</span>
-      </button>
       <button type="button" onClick={abrirMais}>
         <Icone nome="mais" />
         <span>Mais</span>
