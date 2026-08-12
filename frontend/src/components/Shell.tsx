591185868e053ab972860834b84ca8dc45fcae9a
rafaelpaesmeirelles <rafaelpaesmeirelles@gmail.com>
2026-08-12 19:40:34 -0300

    feat(ui): mount route-aware Clinical OS domain context

--- frontend/src/components/Shell.tsx
+++ frontend/src/components/Shell.tsx
@@ -1,11 +1,13 @@
 import ClinicalDesktopNav from "./ClinicalDesktopNav";
 import ClinicalMobileNav from "./ClinicalMobileNav";
 import ClinicalPresentationGuard from "./ClinicalPresentationGuard";
+import ClinicalRouteContext from "./ClinicalRouteContext";
 import ShellClinicalOSLaunch from "./ShellClinicalOSLaunch";
 
 export default function Shell() {
   return (
     <>
+      <ClinicalRouteContext />
       <ClinicalDesktopNav />
       <ShellClinicalOSLaunch />
       <ClinicalMobileNav />
