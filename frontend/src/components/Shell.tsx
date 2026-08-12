d475c107f0dee3020595b6e291573acdac5b66d1
rafaelpaesmeirelles <rafaelpaesmeirelles@gmail.com>
2026-08-12 18:36:37 -0300

    feat(ui): mount canonical mobile navigation

--- frontend/src/components/Shell.tsx
+++ frontend/src/components/Shell.tsx
@@ -1,1 +1,11 @@
-export { default } from "./ShellClinicalOSLaunch";
+import ShellClinicalOSLaunch from "./ShellClinicalOSLaunch";
+import ClinicalMobileNav from "./ClinicalMobileNav";
+
+export default function Shell() {
+  return (
+    <>
+      <ShellClinicalOSLaunch />
+      <ClinicalMobileNav />
+    </>
+  );
+}
