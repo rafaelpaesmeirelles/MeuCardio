1ccc72861a9c5dfcd9275c7958c1f8ea36f977e2
rafaelpaesmeirelles <rafaelpaesmeirelles@gmail.com>
2026-08-18 14:59:52 -0300

    feat(shell): mount daily actions card without touching home structure

--- frontend/src/components/Shell.tsx
+++ frontend/src/components/Shell.tsx
@@ -2,14 +2,17 @@ import ClinicalDesktopNav from "./ClinicalDesktopNav";
 import ClinicalMobileNav from "./ClinicalMobileNav";
 import ClinicalPresentationGuard from "./ClinicalPresentationGuard";
 import ClinicalRouteContext from "./ClinicalRouteContext";
+import HomePendingActionsPortal from "./HomePendingActionsPortal";
 import InvestorDemoBanner from "./InvestorDemoBanner";
 import ShellClinicalOSLaunch from "./ShellClinicalOSLaunch";
+import "../styles/canonical-brand-standard.css";
 
 export default function Shell() {
   return (
     <>
       <ClinicalRouteContext />
       <ClinicalDesktopNav />
+      <HomePendingActionsPortal />
       <ShellClinicalOSLaunch />
       <ClinicalMobileNav />
       <ClinicalPresentationGuard />
