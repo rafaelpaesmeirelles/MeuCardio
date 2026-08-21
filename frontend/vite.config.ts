1d391a6d274d14396ed23c14afc64fbd96f6c049
rafaelpaesmeirelles <rafaelpaesmeirelles@gmail.com>
2026-08-21 12:49:11 -0300

    fix: preservar regra NetworkOnly das APIs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -83,7 +83,7 @@ export default defineConfig({
             }
           },
           {
-            urlPattern: /\/api\/\//,
+            urlPattern: /\/api\//,
             handler: "NetworkOnly"
           }
         ]
